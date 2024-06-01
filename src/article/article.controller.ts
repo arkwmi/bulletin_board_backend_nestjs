efaa1dcbfd79c33208fcbd664a4985fb0cbf974c
arkwmi <codetrain.mai.arakawa@gmail.com>
2024-06-01 11:20:42 +0900

    getArticleDetailのエラーハンドリング追加

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -1,4 +1,10 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  InternalServerErrorException,
+  Param,
+  Post,
+} from '@nestjs/common';
 import { ArticleService } from './article.service';
 import { Article } from './article.entity';
 
@@ -6,24 +12,36 @@ import { Article } from './article.entity';
 export class ArticleController {
   constructor(private readonly articleService: ArticleService) {}
 
-  @Get()
+  @Get() // 記事一覧取得
   async getAllArticles(): Promise<Article[]> {
-    return this.articleService.getAllArticles();
+    try {
+      return await this.articleService.getAllArticles();
+    } catch (error) {
+      console.error('記事一覧の取得に失敗しました:', error);
+      throw new InternalServerErrorException('記事一覧の取得に失敗しました');
+    }
   }
-<<<<<<< Updated upstream
-=======
 
   @Get(':id') // 記事IDと紐づく記事、コメント一覧を取得
   async getArticleDetail(@Param('id') id: string): Promise<Article> {
-    return this.articleService.getArticleDetail(parseInt(id, 10));
+    try {
+      return this.articleService.getArticleDetail(parseInt(id, 10));
+    } catch (error) {
+      console.error('記事詳細の取得に失敗しました:', error);
+      throw new InternalServerErrorException('記事詳細の取得に失敗しました');
+    }
   }
 
-  @Post('post') // 記事登録
+  @Post() // 記事登録
   async postArticle(@Body() createArticleDto: CreateArticleDto) {
-    await this.articleService.postArticle(createArticleDto);
-    return {
-      message: '記事投稿完了',
-    };
+    try {
+      await this.articleService.postArticle(createArticleDto);
+      return {
+        message: '記事投稿完了',
+      };
+    } catch (error) {
+      console.error('記事投稿に失敗しました:', error);
+      throw new InternalServerErrorException('記事投稿に失敗しました');
+    }
   }
->>>>>>> Stashed changes
 }
