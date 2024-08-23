e4f9c2709b55cbbcd414d4e1a8f27a95c2f1364f
arkwmi <codetrain.mai.arakawa@gmail.com>
2024-08-23 18:36:09 +0900

    コミット漏れ

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -38,62 +38,10 @@ export class ArticleController {
     return await this.articleService.getArticlesPerPage(query);
   }
 
+  @Public()
   @Get() // 記事一覧取得
   async getAllArticles(): Promise<Article[]> {
     return await this.articleService.getAllArticles();
-  }
-
-  @Get('exportAll') // 記事全データ取得
-  async exportAllArticles(@Res() res: Response): Promise<void> {
-    try {
-      const csvData = await this.articleService.exportAllArticlesToCSV();
-      console.log(csvData);
-      res.header('Content-Type', 'text/csv');
-      res.attachment('all_articles.csv');
-      res.send(csvData);
-    } catch (error) {
-      console.error('エクスポートエラー:', error);
-      res.status(500).json({ message: 'エクスポート中にエラーが発生しました' });
-    }
-  }
-
-  @Get('exportByDateRange') // 期間指定の記事データ取得
-  async exportArticlesByDateRange(
-    @Res() res: Response,
-    @Query('start_date') startDate: string,
-    @Query('end_date') endDate: string,
-  ): Promise<void> {
-    try {
-      const csvData = await this.articleService.exportArticlesToCSVByDate(
-        startDate,
-        endDate,
-      );
-      console.log(csvData);
-      res.header('Content-Type', 'text/csv');
-      res.attachment('articles_by_date_range.csv');
-      res.send(csvData);
-    } catch (error) {
-      console.error('エクスポートエラー:', error);
-      res.status(500).json({ message: 'エクスポート中にエラーが発生しました' });
-    }
-  }
-
-  @Post('import') // 記事データインポート
-  async importBatch(@Body() body: any) {
-    const batch = body.batch;
-    await this.articleService.importArticles(batch);
-  }
-
-  @Get('search') // 記事検索
-  async searchArticles(@Query('query') query: string): Promise<Article[]> {
-    return this.articleService.searchArticles(query);
-  }
-
-  @UseGuards(AuthGuard)
-  @Get('user-articles') // ログイン中のユーザーが投稿した記事一覧を取得
-  async getArticlesByUserId(@Req() req: Request) {
-    const userId = req['user'].sub; // リクエストからユーザーID取得
-    return await this.articleService.getArticlesByUserId(userId);
   }
 
   @Public()
@@ -111,6 +59,7 @@ export class ArticleController {
     }
   }
 
+  @Public()
   @Get('exportByDateRange') // 期間指定の記事データ取得
   async exportArticlesByDateRange(
     @Res() res: Response,
@@ -132,12 +81,14 @@ export class ArticleController {
     }
   }
 
+  @Public()
   @Post('import') // 記事データインポート
   async importBatch(@Body() body: any) {
     const batch = body.batch;
     await this.articleService.importArticles(batch);
   }
 
+  @Public()
   @Get('search') // 記事検索
   async searchArticles(@Query('query') query: string): Promise<Article[]> {
     return this.articleService.searchArticles(query);
