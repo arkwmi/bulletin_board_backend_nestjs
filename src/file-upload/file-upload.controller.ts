685973014802a3a4dfbb6c8f72374c29bb874938
arkwmi <codetrain.mai.arakawa@gmail.com>
2024-08-31 11:46:48 +0900

    docker設定修正

--- src/file-upload/file-upload.controller.ts
+++ src/file-upload/file-upload.controller.ts
@@ -3,11 +3,8 @@ import {
   Post,
   UploadedFile,
   UseInterceptors,
-  //   BadRequestException,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
-// import { diskStorage } from 'multer';
-// import * as path from 'path';
 import { Public } from 'src/auth/auth.gurad';
 import { FileUploadService } from './file-upload.service';
 
@@ -18,24 +15,6 @@ export class FileUploadController {
   @Public()
   @Post()
   @UseInterceptors(FileInterceptor('file'))
-  // FileInterceptor('file', {
-  //   storage: diskStorage({
-  //     destination: './uploads',
-  //     filename: (_req, file, callback) => {
-  //       const ext = path.extname(file.originalname);
-  //       callback(null, `${Date.now()}${ext}`);
-  //     },
-  //   }),
-  //   fileFilter: (_req, file, callback) => {
-  //     if (!file.mimetype.match(/\/(jpg|jpeg|png|gif)$/)) {
-  //       return callback(
-  //         new BadRequestException('画像データを選択してください'),
-  //         false,
-  //       );
-  //     }
-  //     callback(null, true);
-  //   },
-  // }),
   async uploadFile(@UploadedFile() file: Express.Multer.File) {
     return await this.fileUploadService.uploadFile(file);
   }
--- src/file-upload/file-upload.service.ts
+++ src/file-upload/file-upload.service.ts
@@ -11,19 +11,15 @@ export class FileUploadService {
     this.logger = customLoggerService;
 
     this.minioClient = new Client({
-      endPoint: 'localhost',
+      endPoint: 'minio',
       port: 9000,
       useSSL: false,
-      accessKey: process.env.MINIO_ROOT_USER,
-      secretKey: process.env.MINIO_ROOT_PASSWORD,
+      accessKey: 'minio',
+      secretKey: 'password',
     });
   }
 
   async uploadFile(file: Express.Multer.File) {
-    this.logger.log('入った？');
-    this.logger.log(`File buffer type: ${typeof file.buffer}`);
-    this.logger.log(`File buffer length: ${file.buffer.length}`);
-
     const bucketName = 'images';
     const objectName = `${Date.now()}_${file.originalname}`;
 
